73bfe8226361124c0fc02181b2c237a32c142aa9
SUZUKIyuka <suzukiyuka1218@gmail.com>
2017-04-30 18:07:46 +0900

    Swift3

---
+++ Count/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by nagata on 4/26/17.
+//  Copyright © 2017 nagata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plus() {
+        
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    /* Check.1 マイナス機能 */
+    @IBAction func minus() {
+        
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    /* Check.2 かける・割る機能 */
+    @IBAction func times() {
+        
+        number = number * 2
+        label.text = String(number)
+    }
+
+    @IBAction func divide() {
+        
+        number = number / 2
+        label.text = String(number)
+    }
+
+    
+    
+
+}
+
